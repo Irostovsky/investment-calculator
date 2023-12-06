1608cab815be99e385e372e58d867164a8272456
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2023-12-06 20:42:40 +0200

    Added a user input.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 function App() {
-  return <></>;
+  return (
+    <div id="user-input">
+      <div className="input-group">
+        <div>
+          <label>Initial Investment</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label>Annual Investment</label>
+          <input type="number" />
+        </div>
+      </div>
+      <div className="input-group">
+        <div>
+          <label>Expected Return</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label>Duration</label>
+          <input type="number" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
